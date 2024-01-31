bc1eaa3167a727c5ec73e8da4fa5a1e81dd3b193
xanthe <xanthehorner@gmail.com>
2024-01-31 23:02:31 +0000

    added components and styling

---
+++ portfolio/src/main.jsx
@@ -0,0 +1,13 @@
+
+//import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
